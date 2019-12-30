b89c944cbb547d95226df70b085e8e099220be7d
HaeSeon <gildogi@naver.com>
2019-12-30 17:12:13 +0900

    Create Quick_ver3.c
    version 3: median of three partitioning as a pivot

---
+++ Sort Algorithm Project/Quick_ver3.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<time.h>
+int count_q3 = 0;
+
+
+
+void QuickSort_3(int array[], int p, int r) {
+
+	if (p < r) {
+		count_q3++;
+		int q = Partition_3(array, p, r);
+		QuickSort_3(array, p, q - 1);
+		QuickSort_3(array, q + 1, r);
+	}
+	else {
+		return;
+	}
+}
+
+
+int Partition_3(int array[], int p, int r) {
+	int mid = p + (r - p) / 2;
+	int i;
+	int q = p;
+
+
+	//첫번째, 가운데, 마지막 요소중 중간값을 피벗으로 결정
+	if (array[p] > array[mid]) {
+		swap(&array[p], &array[mid]);
+		count_q3++;
+	}
+	if (array[mid] > array[r]) {
+		swap(&array[mid], &array[r]);
+		count_q3++;
+	}
+	if (array[p] > array[mid]) {
+		swap(&array[p], &array[mid]);
+		count_q3++;
+	}
+	swap(&array[mid], &array[r]);
+	int pivot = array[r];
+
+
+
+	for (i = p; i < r; i++) {
+		if (array[i] <= pivot) {
+			count_q3++;
+			swap(&array[i], &array[q]);
+			q++;
+		}
+	}
+	swap(&array[r], &array[q]);
+
+	return q;
+
+}
+
+int main_Quick_3(int array[], int length) {
+
+	//printf("original array : ");
+	for (int a = 0; a < length; a++) {
+		//printf("%d ", array[a]);
+	}
+	//clock함수를 이용한 실핼시간측정
+	clock_t start, end;
+	start = clock();
+	QuickSort_3(array, 0, length - 1);
+	end = clock();
+	float result = (float)(end - start) / CLOCKS_PER_SEC;
+
+	//printf("\nsorted array : ");
+	for (int a = 0; a < length; a++) {
+		//printf("%d ", array[a]);
+	}
+	printf("\nnumber of key : %d 번", count_q3);
+	printf("\nclock time : %f 초", result);
+
+	return array;
+}
